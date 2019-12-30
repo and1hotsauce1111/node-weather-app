08dbc536aa397b4f65ba90609c54c574a71a8bd6
doritos <doritos@doritosde-iMac.local>
2019-12-30 21:57:09 +0800

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    //console.log('lat', latitude)
+    //console.log('lon', longitude)
+    const url = `https://api.darksky.net/forecast/4b4daabceb47d896326c94bcf12fa97b/${latitude},${longitude}`
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
