0f6e6119626298584c204037192a1cf392abbfa8
doritos <doritos@doritosde-iMac.local>
2019-12-30 23:23:39 +0800

    add about text and forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,21 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    //console.log('lat', latitude)
-    //console.log('lon', longitude)
-    const url = `https://api.darksky.net/forecast/4b4daabceb47d896326c94bcf12fa97b/${latitude},${longitude}`
+  const url = `https://api.darksky.net/forecast/4b4daabceb47d896326c94bcf12fa97b/${latitude},${longitude}?lang=ZH-TW`
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined)
+    } else if (body.error) {
+      callback('Unable to find location', undefined)
+    } else {
+      callback(
+        undefined,
+        `${body.daily.data[0].summary}當前溫度約${body.currently.temperature}度，降雨機率約${body.currently.precipProbability}。
+           紫外線指數為${body.currently.uvIndex}。`
+      )
+    }
+  })
 }
 
 module.exports = forecast
