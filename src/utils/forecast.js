1b1cdc14bb175a5bf87c97394d6918c1229603d5
doritos <doritos@doritosde-iMac.local>
2019-12-30 23:33:38 +0800

    change geocode

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.darksky.net/forecast/4b4daabceb47d896326c94bcf12fa97b/${latitude},${longitude}?lang=ZH-TW`
+  const url = `https://api.darksky.net/forecast/4b4daabceb47d896326c94bcf12fa97b/${latitude},${longitude}?lang=ZH-TW&units=si`
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
